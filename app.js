f221a0eabc4386f3013accde11ebd086e61bfe2d
hha <6631501174@lamduan.mfu.ac.th>
2025-08-26 14:26:57 +0700

    deleteExpense

--- app.js
+++ app.js
@@ -18,3 +18,23 @@ app.get('/password/:pass', (req, res) => {
         res.send(hash);
     });
 });
+
+// ---------- Delete Expense ----------
+app.delete('/expenses/:id', (req, res) => {
+   const expenseId = req.params.id;
+   const userId = req.query.userId;
+   const sql = "DELETE FROM expense WHERE id = ? AND user_id = ?";
+   const params = [expenseId, userId];
+
+
+   con.query(sql, params, (err, result) => {
+       if (err) {
+           console.error(err);
+           return res.status(500).send("Database server error");
+       }
+       if (result.affectedRows === 0) {
+           return res.status(404).send("Expense not found or does not belong to this user");
+       }
+       res.status(200).send("Expense deleted successfully");
+   });
+});
